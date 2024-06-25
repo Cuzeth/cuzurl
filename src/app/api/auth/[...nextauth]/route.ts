02b521723cb00425b5e7a093c7afc97a7bc08f69
Cuzeth <59286294+Cuzeth@users.noreply.github.com>
2024-06-24 19:54:22 -0700

    oops. dont do that.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,8 +4,8 @@ import GitHubProvider from 'next-auth/providers/github';
 export const authOptions: NextAuthOptions = {
     providers: [
         GitHubProvider({
-            clientId: process.env.GITHUB_CLIENT_ID!,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
             authorization: {
                 params: {
                     scope: 'read:user',
