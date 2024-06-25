b43754cf84e7c814a16c2571a6d0a983088d0e15
Cuzeth <59286294+Cuzeth@users.noreply.github.com>
2024-06-25 11:01:56 -0700

    Moved /r?s=x to just /x
    Chur Ethan

--- src/app/[...shortURL]/route.ts
+++ src/app/[...shortURL]/route.ts
@@ -1,11 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
-import pool from '../../../lib/db';
+import pool from '../../lib/db';
 
 export async function GET(req: NextRequest) {
-  const { searchParams } = new URL(req.url);
-  const shortUrl = searchParams.get('s');
-
-  // console.log('Received shortUrl:', shortUrl);
+  const { pathname } = new URL(req.url);
+  const shortUrl = pathname.split('/').pop();
 
   if (!shortUrl) {
     return NextResponse.json({ error: 'Short URL is required' }, { status: 400 });
@@ -17,10 +15,8 @@ export async function GET(req: NextRequest) {
       [shortUrl]
     );
     if (result.rows.length > 0) {
-      // console.log('Redirecting to:', result.rows[0].original_url);
       return NextResponse.redirect(result.rows[0].original_url, 301);
     } else {
-      // console.log('URL not found for shortUrl:', shortUrl);
       return NextResponse.json({ error: 'URL not found' }, { status: 404 });
     }
   } catch (error) {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
       setErrorMessage(data.error);
       setShortUrl('');
     } else {
-      setShortUrl(`${window.location.origin}/api/r?s=${data.shortUrl}`);
+      setShortUrl(`${window.location.origin}/${data.shortUrl}`);
       setErrorMessage('');
     }
   };
