65f85ba2e94c00bbf8d81209a079c143c0640f29
Cuzeth <59286294+Cuzeth@users.noreply.github.com>
2024-06-25 11:19:45 -0700

    Added favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,37 @@
+import { Metadata } from 'next';
 import './globals.css';
 import Provider from './session';
 
-export const metadata = {
-  title: 'URL Shortener',
-  description: 'A simple URL shortener built with Next.js and TypeScript.',
+export const metadata: Metadata = {
+  title: "Personal URL Shortener | Cuzeth",
+  description: "A simple URL Shortener built with Next.JS and TypeScript.",
+  metadataBase: new URL("https://s.cuzeth.com"),
+  icons: [
+    { rel: "icon", url: "/favicon.ico?v=1" },
+    { rel: "apple-touch-icon", url: "/apple-touch-icon.png?v=1", sizes: "180x180" },
+    { rel: "icon", url: "/favicon-32x32.png?v=1", type: "image/png", sizes: "32x32" },
+    { rel: "icon", url: "/favicon-16x16.png?v=1", type: "image/png", sizes: "16x16" },
+    { rel: "manifest", url: "/site.webmanifest?v=1" },
+    { rel: "mask-icon", url: "/safari-pinned-tab.svg?v=1", color: "#dc1616" },
+  ],
+  openGraph: {
+    type: "website",
+    url: "https://s.cuzeth.com/",
+    title: "Personal URL Shortener | Cuzeth",
+    description: "A simple URL Shortener built with Next.JS and TypeScript.",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Personal URL Shortener | Cuzeth",
+    description: "A simple URL Shortener built with Next.JS and TypeScript.",
+  },
+  authors: [
+    {
+      name: "Cuzeth",
+      url: "https://s.cuzeth.com",
+    },
+  ],
+  manifest: "/site.webmanifest?v=1",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
