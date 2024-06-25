6f887d9c5c0ec5f2a5cbf4dc3c44834b0e9f4a84
Cuzeth <59286294+Cuzeth@users.noreply.github.com>
2024-06-24 21:20:50 -0700

    Moved redirect to r and made shortURL be s to make the url actually short

--- src/app/api/r/route.ts
+++ src/app/api/r/route.ts
@@ -3,7 +3,7 @@ import pool from '../../../lib/db';
 
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
-  const shortUrl = searchParams.get('shortUrl');
+  const shortUrl = searchParams.get('s');
 
   console.log('Received shortUrl:', shortUrl);
 
